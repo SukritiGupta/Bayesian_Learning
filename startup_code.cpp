fe6c5e1b141dba87701318351cc1feb2c25ec1c4
ansh2233 <ansh.sapra2233@gmail.com>
2019-10-17 15:56:34 +0530

    read dataset and changed fields of classes ro public

--- startup_code.cpp
+++ startup_code.cpp
@@ -5,15 +5,21 @@
 #include <fstream>
 #include <sstream>
 #include <cstdlib>
+#include <iterator>
+#include <algorithm>
+
+// #include <random>
 
 
 // Format checker just assumes you have Alarm.bif and Solved_Alarm.bif (your file) in current directory
 using namespace std;
 
+// allocates things on heap
+vector<vector<int>> dataset;
+
 // Our graph consists of a list of nodes where each node is represented as follows:
 class Graph_Node{
-
-private:
+public:
 	string Node_Name;  // Variable name 
 	vector<int> Children; // Children of a particular node - these are index of nodes in graph.
 	vector<string> Parents; // Parents of a particular node- note these are names of parents
@@ -21,7 +27,7 @@ private:
 	vector<string> values; // Categories of possible values
 	vector<float> CPT; // conditional probability table as a 1-d array . Look for BIF format to understand its meaning
 
-public:
+// public:
 	// Constructor- a node is initialised with its name and its categories
     Graph_Node(string name,int n,vector<string> vals)
 	{
@@ -85,10 +91,8 @@ public:
 
  // The whole network represted as a list of nodes
 class network{
-
-	list <Graph_Node> Pres_Graph;
-
 public:
+	list <Graph_Node> Pres_Graph;
 	int addNode(Graph_Node node)
 	{
 		Pres_Graph.push_back(node);
@@ -113,7 +117,7 @@ public:
         }
         return -1;
     }
-// get the node at nth index
+    // get the node at nth index
     list<Graph_Node>::iterator get_nth_node(int n)
     {
        list<Graph_Node>::iterator listIt;
@@ -143,9 +147,20 @@ public:
 
 };
 
-network read_network()
-{
-	network Alarm;
+network Alarm;
+
+
+
+template<typename T>
+void printVector(const T& t) {
+    cout << "[ ";
+    std::copy(t.cbegin(), t.cend(), std::ostream_iterator<typename T::value_type>(std::cout, ", "));
+    cout << "]" << endl;
+} 
+
+
+network read_network(){
+	network Alarm1;
 	string line;
 	int find=0;
   	ifstream myfile("alarm.bif"); 
@@ -153,119 +168,139 @@ network read_network()
   	string name;
   	vector<string> values;
   	
-    if (myfile.is_open())
-    {
-    	while (! myfile.eof() )
-    	{
+    if (myfile.is_open()){
+    	while (! myfile.eof() ){
     		stringstream ss;
       		getline (myfile,line);
-      		
       		
       		ss.str(line);
      		ss>>temp;
      		
-     		
-     		if(temp.compare("variable")==0)
-     		{
-                    
-     				ss>>name;
-     				getline (myfile,line);
-                   
-     				stringstream ss2;
-     				ss2.str(line);
-     				for(int i=0;i<4;i++)
-     				{
-     					
-     					ss2>>temp;
-     					
-     					
-     				}
-     				values.clear();
-     				while(temp.compare("};")!=0)
-     				{
-     					values.push_back(temp);
-     					
-     					ss2>>temp;
-    				}
-     				Graph_Node new_node(name,values.size(),values);
-     				int pos=Alarm.addNode(new_node);
-
-     				
+     		if(temp.compare("variable")==0){
+ 				ss>>name;
+ 				getline (myfile,line);
+               
+ 				stringstream ss2;
+ 				ss2.str(line);
+ 				for(int i=0;i<4;i++){
+ 					ss2>>temp;
+				}
+ 				values.clear();
+ 				while(temp.compare("};")!=0)
+ 				{
+ 					values.push_back(temp);
+ 					
+ 					ss2>>temp;
+				}
+ 				Graph_Node new_node(name,values.size(),values);
+ 				int pos=Alarm1.addNode(new_node);
      		}
-     		else if(temp.compare("probability")==0)
-     		{
-                    
-     				ss>>temp;
-     				ss>>temp;
-     				
-                    list<Graph_Node>::iterator listIt;
-                    list<Graph_Node>::iterator listIt1;
-     				listIt=Alarm.search_node(temp);
-                    int index=Alarm.get_index(temp);
-                    ss>>temp;
-                    values.clear();
-     				while(temp.compare(")")!=0)
-     				{
-                        listIt1=Alarm.search_node(temp);
-                        listIt1->add_child(index);
-     					values.push_back(temp);
-     					
-     					ss>>temp;
-
-    				}
-                    listIt->set_Parents(values);
-    				getline (myfile,line);
-     				stringstream ss2;
-                    
-     				ss2.str(line);
-     				ss2>> temp;
-                    
-     				ss2>> temp;
-                    
-     				vector<float> curr_CPT;
-                    string::size_type sz;
-     				while(temp.compare(";")!=0)
-     				{
-                        
-     					curr_CPT.push_back(atof(temp.c_str()));
-     					
-     					ss2>>temp;
-                       
-                        
-
-    				}
-                    
-                    listIt->set_CPT(curr_CPT);
-
-
+     		else if(temp.compare("probability")==0){
+ 				ss>>temp;
+ 				ss>>temp;
+ 				
+                list<Graph_Node>::iterator listIt;
+                list<Graph_Node>::iterator listIt1;
+ 				listIt=Alarm1.search_node(temp);
+                int index=Alarm1.get_index(temp);
+                ss>>temp;
+                values.clear();
+ 				while(temp.compare(")")!=0){
+                    listIt1=Alarm1.search_node(temp);
+                    listIt1->add_child(index);
+ 					values.push_back(temp);
+ 					ss >> temp;
+				}
+                listIt->set_Parents(values);
+				getline (myfile,line);
+ 				stringstream ss2;
+ 				ss2.str(line);
+ 				ss2>> temp;
+ 				ss2>> temp;
+ 				vector<float> curr_CPT;
+                string::size_type sz;
+ 				while(temp.compare(";")!=0){
+                    // curr_CPT.push_back(atof(temp.c_str()));
+ 					float random_num = ((float)(rand()%100) +1)/101.0;
+                    curr_CPT.push_back(random_num);
+ 					ss2>>temp;
+				}
+                listIt->set_CPT(curr_CPT);
      		}
-            else
-            {
-                
+            else{
+                // NOTHING
             }
-     		
-     		
-
-    		
-    		
     	}
     	
     	if(find==1)
     	myfile.close();
   	}
   	
-  	return Alarm;
+  	return Alarm1;
 }
+
+void read_dataset(int num_vars, vector<int> *indexes){
+    string line;
+    ifstream myfile("records.dat");
+    while(!myfile.eof()){
+        stringstream ss;
+        vector<int> vec(num_vars);
+        string temp;
+        getline(myfile, line);
+        ss.str(line);
+        bool found_q = false;
+        list<Graph_Node>::iterator gg = Alarm.Pres_Graph.begin();
+        for(int i=0; i<num_vars; i++){
+            ss >> temp;
+            if(temp.compare("\"?\"")==0){
+                found_q = true;
+                indexes->push_back(i);
+            }
+            else{
+                vector<string>::iterator valuesIT;
+                int ind = 0;
+                for(valuesIT=gg->values.begin(); valuesIT<gg->values.end(); valuesIT++){
+                    if(valuesIT->compare(temp)==0){
+                        vec[i] = ind;
+                        // cout << "READ [" << temp << "] as " << vec[i] << " from values";
+                        // printVector(gg->get_values());  
+                        break;
+                    }
+                    // TODO: remove this (not needed) (just for debugging)
+                    if(ind==gg->nvalues-1){
+                        cout << "ERROR: called find with [" << temp << "] and values ";
+                        printVector(gg->get_values());
+                    }
+                    ind++;
+                }
+            }
+            std::advance(gg, 1);        
+        }
+        if(!found_q){
+            indexes->push_back(-1);
+        }
+        dataset.push_back(vec);
+    }
+}
+
 
 
 int main()
 {
-	network Alarm;
 	Alarm=read_network();
     
-// Example: to do something
-	cout<<"Perfect! Hurrah! \n";
+    // Example: to do something
+	cout<<"Perfect! Hurrah! \n" << endl;
 	
+    list<Graph_Node>::iterator listIt;
+    // for(listIt=Alarm.Pres_Graph.begin();listIt!=Alarm.Pres_Graph.end();listIt++){
+    //     printVector(listIt->get_values());
+    //     // cout << listIt->get_Parents().size() << " " << endl;
+    // }
+
+    vector<int> q_indexes;
+    read_dataset(Alarm.Pres_Graph.size(), &q_indexes);
+
 }
 
 
